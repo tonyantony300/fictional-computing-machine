061a4a66fece620e15417ed58f04219823d17fdf
Tony personal <tonyantony300@gmail.com>
2023-03-09 17:40:22 +0530

    first day commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template class="h-screen">
+  <!-- <body class="container-lg mx-10 my-10 bg-gray-600">
+    <div class="text-3xl font-arial bg-red-600 flex border-8 border-green-500 justify-between items-center"
+      style="color: black !important">
+  <div class="pl-4">Invoices</div>
+  <div class="pr-4 pl-2 bg-blue-600 ">New Invoice</div>
+    </div>
+  </body> -->
+
+<!-- <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+    <div class="flex items-center mr-6">
+      <img src="./assets/images/logo.png" alt="Logo" class="h-6">
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4">
+          Home
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4">
+          About
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200">
+          Contact
+          </a>
+        </div>
+      </div>
+    </nav> -->
+
+  <div class="bg-gray-300 h-screen">
+    <div class="container w-full bg-green-300 mx-auto px-0 lg:px-0">
+      <header class="bg-gray-500 text-white py-5 lg:py-10">
+        <h1 class="text-center text-xl font-bold text-red-300 lg:text-3xl">My Website</h1>
+      </header>
+      <main class="py-8 lg:py-12">
+        <p class="text-lg lg:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet commodo
+          ex, vitae luctus orci maximus id. Donec vitae nulla fermentum, cursus mauris eu, viverra nisl. Phasellus at
+          mauris tortor. Proin pellentesque purus ac neque gravida, non suscipit augue ultricies. Vivamus pulvinar euismod
+          turpis, a faucibus nisl fermentum ut.</p>
+      </main>
+      <footer class="bg-gray-500 text-white py-5 lg:py-8">
+        <p class="text-center text-sm lg:text-base">Copyright &copy; 2021</p>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
+
