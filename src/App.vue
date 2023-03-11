01508b9f96863a0a193652ca519e371db7bdfa7a
TONY ANTONY <tonyantony300@gmail.com>
2023-03-11 16:19:46 +0530

    3d block addition

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup></script>
+<!-- <script setup></script> -->
 
 <!-- <template >
   <div> -->
@@ -56,25 +56,25 @@
   </div>
 </template> -->
 
-<template>
+<!-- <template> -->
   <!-- <input v-model="num1" type="number" placeholder="num 1" />
   <input v-model="num2" type="number" placeholder="num 2" /> -->
-  <div class="bg-gray-700 h-screen">
-    <div class="flex flex-wrap">
+  <!-- <div class="bg-gray-700 h-screen">
+    <div class="flex flex-wrap"> -->
       <!-- <br />
       num1 =>{{ num1 }}
       <br />
       num2 =>{{ num2 }}
       <br />
       res => {{ result }} -->
-      <div
+      <!-- <div
         v-for="(item, index) in selected"
         class="w-1/4 border border-gray-500 text-center py-2"
         :class="item"
         style="padding-top: 5%; padding-bottom: 5%"
       >
         {{ index + 1 }}
-      </div>
+      </div> -->
       <!-- <div class="w-1/4  border border-gray-500 text-center py-2 " :class="selected.length > 0 ? selected[0] : 'bg-red-300'"
   style=" padding-top: 5%; padding-bottom: 5%;">1</div> -->
       <!-- <div class="w-1/4  border border-gray-500 text-center py-2 bg-blue-300" :class="selected[1]"
@@ -99,7 +99,7 @@
     style=" padding-top: 5%; padding-bottom: 5%;">11</div>
   <div class="w-1/4  border border-gray-500 text-center py-2 bg-emerald-300" :class="selected[11]"
     style=" padding-top: 5%; padding-bottom: 5%;">12</div> -->
-    </div>
+    <!-- </div>
     <div class="flex justify-center items-center py-10">
       <button
         class="bg-blue-500 hover:bg-blue-800 hover:text-green-400 font-bold px-6 py-1 m-9 rounded"
@@ -110,9 +110,9 @@
       </button>
     </div>
   </div>
-</template>
+</template> -->
 
-<script>
+<!-- <script>
 export default {
   name: "App",
   data() {
@@ -158,7 +158,7 @@ export default {
         .map((i) => {
           let randomness = Math.floor(Math.random() * 7);
           this.selected.push(this.colors[randomness]);
-        });
+        }); -->
       // let randomness1 = Math.floor(Math.random() * 7)
       // this.selected[0] = this.colors[randomness1]
       // let randomness2 = Math.floor(Math.random() * 7)
@@ -198,6 +198,174 @@ export default {
       //   }
       //   console.log(this.count)
       // },
+//     },
+//   },
+// };
+<!-- </script> -->
+
+<template>
+  <div>
+    <div ref="container"></div>
+    <input type="range" v-model="rotation" min="0" max="360" />
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+function createTexture(text) {
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      const width = 512;
+      const height = 512;
+
+      canvas.width = width;
+      canvas.height = height;
+      context.fillStyle = "#ffffff";
+      context.fillRect(0, 0, width, height);
+      context.font = "Bold 48px Arial";
+      context.fillStyle = "#000000";
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(text, width / 2, height / 2);
+
+      const texture = new THREE.Texture(canvas);
+      texture.needsUpdate = true;
+      return texture;
+    }
+
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      rotation: 0,
+      zoomLevel: 0,
+    };
+  },
+  mounted() {
+    // create the 3D scene
+    const scene = new THREE.Scene();
+
+    // create the camera
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 5);
+
+    // create the renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.$refs.container.appendChild(renderer.domElement);
+
+    // create the cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const materials = [
+      new THREE.MeshBasicMaterial({ color: 0xF5F5F5 ,  map: createTexture("Tony") ,  side: THREE.DoubleSide }),
+      new THREE.MeshBasicMaterial({ color: 0xC7EBFF , map: createTexture("Sandra") ,  side: THREE.DoubleSide  }),
+      new THREE.MeshBasicMaterial({ color: 0x222222 , map: createTexture("Haritha")  ,  side: THREE.DoubleSide  }),
+      new THREE.MeshBasicMaterial({ color: 0xCCCCCC , map: createTexture("Abru") ,  side: THREE.DoubleSide }),
+      new THREE.MeshBasicMaterial({ color: 0x777777 , map: createTexture("Jkk") ,  side: THREE.DoubleSide   }),
+      new THREE.MeshBasicMaterial({ color: 0x00ffff , map: createTexture("Chinju") ,  side: THREE.DoubleSide }),
+    ];
+    const cube = new THREE.Mesh(geometry, materials);
+    scene.add(cube);
+
+       // Handle zoom event
+       camera.addEventListener("zoom", (event) => {
+        // Get new zoom level
+        const newZoomLevel = camera.zoom;
+        if (newZoomLevel === this.zoomLevel) {
+          return;
+        }
+        console.log(newZoomLevel)
+        this.zoomLevel = newZoomLevel;
+        const textureLoader = new THREE.TextureLoader();
+        switch (Math.floor(newZoomLevel)) {
+          case 0:
+            cube.material[0].map = textureLoader.load(
+              "https://picsum.photos/id/237/400/400"
+            );
+            cube.material[1].map = textureLoader.load(
+              "https://picsum.photos/id/238/400/400"
+            );
+            cube.material[2].map = textureLoader.load(
+              "https://picsum.photos/id/239/400/400"
+            );
+            cube.material[3].map = textureLoader.load(
+              "https://picsum.photos/id/239/400/400");
+              break;
+          case 1:
+          cube.material[0].map = textureLoader.load(
+              "https://picsum.photos/id/240/400/400"
+            );
+            cube.material[1].map = textureLoader.load(
+              "https://picsum.photos/id/241/400/400"
+            );
+            cube.material[2].map = textureLoader.load(
+              "https://picsum.photos/id/242/400/400"
+            );
+            cube.material[3].map = textureLoader.load(
+              "https://picsum.photos/id/243/400/400"
+            );
+            break;
+            case 2:
+            cube.material[0].map = textureLoader.load(
+              "https://picsum.photos/id/244/400/400"
+            );
+            cube.material[1].map = textureLoader.load(
+              "https://picsum.photos/id/245/400/400"
+            );
+            cube.material[2].map = textureLoader.load(
+              "https://picsum.photos/id/246/400/400"
+            );
+            cube.material[3].map = textureLoader.load(
+              "https://picsum.photos/id/247/400/400"
+            );
+            break;
+            case 3:
+            cube.material[0].map = textureLoader.load(
+              "https://picsum.photos/id/248/400/400"
+            );
+            cube.material[1].map = textureLoader.load(
+              "https://picsum.photos/id/249/400/400"
+            );
+            cube.material[2].map = textureLoader.load(
+              "https://picsum.photos/id/250/400/400"
+            );
+            cube.material[3].map = textureLoader.load(
+              "https://picsum.photos/id/251/400/400"
+            );
+            break;
+        }
+      });
+
+         // Handle window resize
+         window.addEventListener("resize", () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      });
+
+    // create the controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // animate the scene
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+      cube.rotation.z += 0.005;
+      controls.update();
+      renderer.render(scene, camera);
+    };
+    animate();
+    
+  },
+  watch: {
+    rotation(value) {
+      // update the rotation of the cube
+      const cube = this.$el.querySelector('mesh');
+      cube.rotation.y = value * Math.PI / 180;
     },
   },
 };
