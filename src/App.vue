5e8ed046202ccf70ec611daee51f979405a29999
Tony personal <tonyantony300@gmail.com>
2023-03-16 12:45:14 +0530

    started new project named quizzes

--- src/App.vue
+++ src/App.vue
@@ -443,7 +443,7 @@ export default {
   </div>
 </div> -->
 
-<script setup>
+<!-- <script setup>
 import { ref } from "vue";
 const showModal = ref(false);
 const newNote = ref("");
@@ -603,5 +603,85 @@ header button {
 .modal .close {
   background-color: rgb(193, 15, 15);
   margin-top: 7px;
+}
+</style> -->
+
+// Quiz app
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search....." />
+    </header>
+    <div class="options-container">
+      <div class="card">
+        <img
+          src="https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg"
+          alt=""
+        />
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  font-size: 20px;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
 }
 </style>
