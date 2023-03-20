15ae19f423bb1af0679255553aeeb5a8da6eec4a
Tony personal <tonyantony300@gmail.com>
2023-03-20 12:36:00 +0530

    friday and monday commit

--- src/components/icons/Cards.vue
+++ src/components/icons/Cards.vue
@@ -1,11 +1,20 @@
+<script setup>
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
+import router from "../../router";
+const { quiz } = defineProps(["quiz"]);
+
+const navigateToQuiz = () => {
+  router.push(`/quiz/${quiz.id}`);
+};
+</script>
+
 <template>
-  <div class="card">
-    <img
-      src="https://marvel-b1-cdn.bc0a.com/f00000000026007/resilienteducator.com/wp-content/uploads/2012/11/AdobeStock_60467600_cup.jpg"
-    />
+  <div class="card" @click="navigateToQuiz">
+    <img :src="quiz.img" />
     <div class="card-text">
-      <h2>Math</h2>
-      <p>3 questions</p>
+      <h2>{{ quiz.name }}</h2>
+      <p>{{ quiz.questions.length }} questions</p>
     </div>
   </div>
 </template>
